953c6161a112e28985226d6bd3e8c88207d0ba0b
Anton Sanzharovskyi <30695789+avs123a@users.noreply.github.com>
2018-04-27 18:25:10 +0300

    Update logout.php

--- logout.php
+++ logout.php
@@ -2,11 +2,9 @@
 session_start();
 if(isset($_POST['logout_btn']))
 {
-	//unset($_COOKIE['AVS']);
 	unset($_SESSION['user_email']);
 	unset($_SESSION['user_real_name']);
 	session_unset();
-	//unset($_COOKIE['PHPSESSID']); //delete session coockies
 	header("Location: login.html");
 	exit;
 }
