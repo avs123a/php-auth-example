37b0e24b0d68fcd20217b4140c7c06ef557a9d0e
Anton Sanzharovskyi <30695789+avs123a@users.noreply.github.com>
2018-04-27 18:24:09 +0300

    Update signup.php

--- signup.php
+++ signup.php
@@ -20,37 +20,41 @@ $db = new \ova777\MYSQLi\Connection($host, $user, $password, $database);
 $countries = $db->command('SELECT * FROM country')->queryAll();
 
 ?>
+
 <!DOCTYPE HTML>
 <html>
-    <head>
-        <title>Register</title>
-    </head>
-    <body>
-	    <h2>Register</h2>
-        <form name="register-form" action="/signup_procedure.php" method="post">
-            <label for="email-field">Email: </label>
-            <input type="text" id="email-field" name="user_email" required /><br>
-            <label for="login-field">Login: </label>
-            <input type="text" id="login-field" name="user_login" required/><br>
-            <label for="rname-field">Real name: </label>
-            <input type="text" id="rname-field" name="user_rname" required/><br>
-            <label for="passw-field">Password: </label>
-            <input type="password" id="passw-field" name="user_password" required/><br>
-	    	<label for="bdate-field">Birth date : </label>
-            <input type="date" id="bdate-field" name="user_bdate" required/><br>
-	    	<label for="country-field">Country : </label>
-	    	<select name="user_country" id="country-field" required>
-	    	    <option value="prompt" disabled selected>Choose country</option>
-	    		<?php foreach($countries as $country): ?>
-		    	    
-		    		<option value="<?= $country['id'] ?>"><?= $country['name'] ?></option>
+	<head>
+		<title>Register</title>
+	</head>
+	<body>
+		<h2>Register</h2>
+		<form name="register-form" action="/signup_procedure.php" method="post">
+			<label for="email-field">Email: </label>
+			<input type="text" id="email-field" name="user_email" required /><br>
+			<label for="login-field">Login: </label>
+			<input type="text" id="login-field" name="user_login" required/><br>
+			<label for="rname-field">Real name: </label>
+			<input type="text" id="rname-field" name="user_rname" required/><br>
+			<label for="passw-field">Password: </label>
+			<input type="password" id="passw-field" name="user_password" required/><br>
+			<label for="bdate-field">Birth date : </label>
+			<input type="date" id="bdate-field" name="user_bdate" required/><br>
+			<label for="country-field">Country : </label>
+			<select name="user_country" id="country-field" required>
+				<option value="prompt" disabled selected>Choose country</option>
+				
+				<?php foreach($countries as $country): ?>
+				
+				<option value="<?= $country['id'] ?>"><?= $country['name'] ?></option>
 		    		
-		    	<?php endforeach; ?>
-	    	</select><br>
-	    	<p>
-	    	    <input type="checkbox" name="user_agreed" required> I agree with terms and conditions.
-	    	</p><br>
+				<?php endforeach; ?>
+			</select>
+			<br>
+			<p>
+				<input type="checkbox" name="user_agreed" required> I agree with terms and conditions.
+			</p>
+			<br>
 			<input type="submit" name="user_register" value="Sign UP" />
-        </form>
-    </body>
+		</form>
+	</body>
 </html>
